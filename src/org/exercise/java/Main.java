a6440d32f105cd757cfe36714d784ad79adb17fe
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-03-14 16:06:03 +0100

    Add:User questions

--- src/org/exercise/java/Main.java
+++ src/org/exercise/java/Main.java
@@ -1,25 +1,47 @@
 package org.exercise.java;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Book libro = null;
+
+        //set bookshelf lenght
+        System.out.print("Quanti libri vuoi aggiungere: ");
+        int arrayLength = Integer.parseInt(input.nextLine());
+        Book[] bookshelf = new Book[arrayLength];
+        System.out.print(Arrays.toString(bookshelf));
+
+        System.out.print("Bene scegli un libro: ");
+        String title = input.nextLine();
+        System.out.print("Quante pagine ha: ");
+        int pageNum = Integer.parseInt(input.nextLine());
+        System.out.print("Dimmi il nome dell' autore:  ");
+        String author = input.nextLine();
+        System.out.print("Qual'è la casa editrice ");
+        String publisher = input.nextLine();
+
+
         try {
-            libro = new Book("Boccamara", 3, "Luigi", "GGG");
+            libro = new Book(title, pageNum, author, publisher);
+            System.out.println(libro.toString());
         } catch (Exception e) {
-            e.printStackTrace();
+            e.getMessage();
         }
 
-        try {
-            libro.setAuthor("dio");
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            libro.setPageNumber(10);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            libro.setAuthor("dio");
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            libro.setPageNumber(10);
+//        } catch (RuntimeException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
